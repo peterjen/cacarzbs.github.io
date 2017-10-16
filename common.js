78ccb3d9897f18ea64fae0f904e0ed842e99d874
peterjen <peter.jen@gmail.com>
2017-10-15 21:11:26 -0400

    RZBS_V1

---
+++ common.js
@@ -0,0 +1,22 @@
+var cars = [];
+var i = 0;
+var text = "";
+
+function getNumbers(m){
+alert(m);
+	while (cars.length < m) {
+		var x = Math.floor((Math.random() * m +1));
+		if (cars.indexOf(x)<0){
+			cars.push(x);
+		}
+	}
+}
+
+
+function myFunction() {
+    i++;
+    var x = cars[i];
+alert(x);
+    ii='<img src=".\\level 1T\\Slide'+x+'.JPG" >';
+    document.getElementById('show1').innerHTML = ii;
+}
