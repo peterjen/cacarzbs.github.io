05dc6f2efe38321da75121378b49fadb9cd2662d
peterjen <peter.jen@gmail.com>
2017-10-15 21:24:15 -0400

    RZBS_V1

--- common.js
+++ common.js
@@ -3,7 +3,6 @@ var i = 0;
 var text = "";
 
 function getNumbers(m){
-alert(m);
 	while (cars.length < m) {
 		var x = Math.floor((Math.random() * m +1));
 		if (cars.indexOf(x)<0){
@@ -16,7 +15,6 @@ alert(m);
 function myFunction() {
     i++;
     var x = cars[i];
-alert(x);
     ii='<img src=".\\level 1T\\Slide'+x+'.JPG" >';
     document.getElementById('show1').innerHTML = ii;
 }
