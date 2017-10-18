347ec89459d0e68768c0440670e07de0af6508d5
peterjen <peter.jen@gmail.com>
2017-10-17 21:50:59 -0400

    upload 20171017

--- safe.js
+++ safe.js
@@ -1,29 +1,9 @@
-var cars = [];
-var i = 0;
-var text = "";
-
-function getNumbers(m){
-	while (cars.length < m) {
-		var x = Math.floor((Math.random() * m +1));
-		if (cars.indexOf(x)<0){
-			cars.push(x);
-		}
-	}
-}
-
-
-function myFunction(b) {
-    i++;
-    var x = cars[i];
-    ii='<img src=".\\level '+b+'T\\Slide'+x+'.JPG" >';
-    document.getElementById('show1').innerHTML = ii;
-}
-
 function disableClick(){
 	
 	document.onmousedown=function(event){
           if ((window.event.button == 2) || (event.button == 2) ) {
-            alert('Welcome to RZBS');
+            alert('Welcome to RZBS 1');
+	    location.href='http://www.google.com';
             return false;
           }
 	  event = (event || window.event);
@@ -35,8 +15,8 @@ function disableClick(){
 
         document.onclick=function(event){
           if (event.button == 2) {
-                alert('Welcome to RZBS');
-                location.href='./index.html';
+                alert('Welcome to RZBS 2');
+                location.href='http://www.google.com';
                 return false;
           }
 
@@ -59,20 +39,4 @@ function disableClick(){
           }
           return false;	
 	}
-}
-
-function disableClick_old(){
-	document.onmousedown=function(event){
-          if ((window.event.button == 2) || (event.button == 2) ) {
-            alert('RZBS');
-            return false;
-          }
-        }
-        document.onclick=function(event){
-          if (event.button == 2) {
-                alert('RZBS');
-                location.href='./index.html';
-                return false;
-          }
-        }
 }
